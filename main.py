11dc9218797b302d3c393bdf58018554f9be19b4
Martin Boyanov <martin.boyanov@komfo.com>
2015-06-11 18:55:26 +0300

    Populating the map

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 import BeautifulSoup
+import collections
 html_doc = requests.get("http://schedules.sofiatraffic.bg")
-#print html_doc.text.encode('ISO-8859-1')
-#print html_doc.encoding
 soup = BeautifulSoup.BeautifulSoup(html_doc.text.encode('ISO-8859-1'))
 transports=[]
 for link in soup.findAll('a'):
@@ -11,3 +10,21 @@ for link in soup.findAll('a'):
 
 import pprint
 pprint.pprint(transports)
+
+karta = collections.defaultdict(lambda: [])
+for transport in transports:
+	url  = "http://schedules.sofiatraffic.bg/"+transport
+	print url
+	html_doc = requests.get(url)
+	soup = BeautifulSoup.BeautifulSoup(html_doc.text.encode('ISO-8859-1'))
+	routes = soup.findAll('ul', { "class" : "schedule_direction_signs" })[:2]
+
+	for route in routes:
+		last_stop = None
+		stops = route.findAll('li')
+		for stop in stops:
+			current_stop = stop['class']
+			current_stop = current_stop[current_stop.find("stop_")+5:]
+			if last_stop:
+				karta[last_stop].append(current_stop)
+			last_stop = current_stop
